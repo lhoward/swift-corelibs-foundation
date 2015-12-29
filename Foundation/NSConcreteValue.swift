2f6569d0ef179b823ec569f3947d2cc9c0f43fe5
Luke Howard <lukeh@padl.com>
2015-12-29 22:32:24 +1100

    [SR-377] Implement NSConcreteValue class
    The NSConcreteValue class is a wrapper for arbitrary values that can be
    described using Objective-C type notation. Note that owing to the lack of
    factory initializers, a rather ugly workaround is used to make NSValue
    behave as a class cluster. This should not have a performance impact in
    practice as its init() is a NOOP and most NSValues will be NSNumbers.
    
    NSConcreteValue does not yet support "special" values such as NSRects.
    An implementation will be forthcoming in a separate commit.
    
    use nslog

---
+++ Foundation/NSConcreteValue.swift
@@ -0,0 +1,199 @@
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2014 - 2015 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See http://swift.org/LICENSE.txt for license information
+// See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+
+final internal class NSConcreteValue : NSObject, NSCopying, NSSecureCoding, NSCoding {
+    enum SpecialFlags : Int {
+        case NotSpecialType = 0
+    }
+    
+    struct TypeInfoHeader : Equatable {
+        let size : Int
+        let name : String
+
+        init(size: Int, withName name: String) {
+            self.size = size
+            self.name = name
+        }
+    }
+
+    typealias TypeInfo = ManagedBuffer<TypeInfoHeader, Int8>
+    
+    static var _cachedTypeInfo = Dictionary<String, TypeInfo>()
+    
+    var _specialFlags : SpecialFlags = .NotSpecialType
+    var _typeInfo : TypeInfo
+    var _value : UnsafeMutablePointer<UInt8>
+    
+    static func createTypeInfoNonSpecial(spec: String, objCType typep: UnsafePointer<Int8>) -> TypeInfo? {
+        var size: Int = 0
+        var align: Int = 0
+        var count : Int = 0
+        let extraBytes : Int = spec.length + 1 // extra bytes after TypeInfoHeader
+        
+        var type = NSObjCType(spec)
+        guard type != nil else {
+            print("NSConcreteValue.createTypeInfoNonSpecial: unsupported type encoding spec '\(spec)'")
+            return nil
+        }
+        
+        if type == .StructBegin {
+            fatalError("NSConcreteValue.createTypeInfoNonSpecial: cannot encode structs")
+        } else if type == .ArrayBegin {
+            let scanner = NSScanner(string: spec)
+            
+            scanner.scanLocation = 1
+            
+            guard scanner.scanInteger(&count) && count > 0 else {
+                print("NSKeyedUnarchiver.decodeValueOfObjCType: array count is missing or zero")
+                return nil
+            }
+            
+            guard let elementType = _NSSimpleObjCType(scanner.scanUpToString(String(_NSSimpleObjCType.ArrayEnd))) else {
+                print("NSKeyedUnarchiver.decodeValueOfObjCType: array type is missing")
+                return nil
+            }
+            
+            guard _NSGetSizeAndAlignment(elementType, &size, &align) else {
+                print("NSConcreteValue.createTypeInfoNonSpecial: unsupported type encoding spec '\(spec)'")
+                return nil
+            }
+            
+            type = elementType
+        }
+
+        guard _NSGetSizeAndAlignment(type!, &size, &align) else {
+            print("NSConcreteValue.createTypeInfoNonSpecial: unsupported type encoding spec '\(spec)'")
+            return nil
+        }
+
+        let typeInfo = TypeInfo.create(extraBytes, initialValue: {(buffer: ManagedProtoBuffer<TypeInfoHeader, Int8>) -> TypeInfoHeader in
+            buffer.withUnsafeMutablePointers({(value: UnsafeMutablePointer<NSConcreteValue.TypeInfoHeader>,
+                                               typedata: UnsafeMutablePointer<Int8>) -> TypeInfoHeader in
+                typedata.initializeFrom(unsafeBitCast(typep, UnsafeMutablePointer<Int8>.self), count: extraBytes)
+                if count != 0 {
+                    size *= count // is an array
+                }
+                return TypeInfoHeader(size: size, withName: spec)
+            })
+        })
+        
+        return typeInfo
+    }
+ 
+    static func createTypeInfo(name: String, objCType typep: UnsafePointer<Int8>) -> TypeInfo? {
+        return createTypeInfoNonSpecial(name, objCType: typep)
+    }
+    
+    required init(bytes value: UnsafePointer<Void>, objCType type: UnsafePointer<Int8>) {
+        let spec = String.fromCString(type)!
+
+        var typeInfo = NSConcreteValue._cachedTypeInfo[spec]
+        if typeInfo == nil {
+            typeInfo = NSConcreteValue.createTypeInfo(spec, objCType: type)
+            guard typeInfo != nil else {
+                fatalError("NSConcreteValue.init: failed to initialize from type encoding spec '\(spec)'")
+            }
+            NSConcreteValue._cachedTypeInfo[spec] = typeInfo
+        }
+
+        self._typeInfo = typeInfo!
+        self._value = UnsafeMutablePointer<UInt8>.alloc(self._typeInfo.value.size)
+        if value != nil {
+            self._value.initializeFrom(unsafeBitCast(value, UnsafeMutablePointer<UInt8>.self), count: self._typeInfo.value.size)
+        }
+        
+        super.init()
+    }
+ 
+    deinit {
+        self._value.destroy(self._size)
+        self._value.dealloc(self._size)
+    }
+    
+    func getValue(value: UnsafeMutablePointer<Void>) {
+        UnsafeMutablePointer<UInt8>(value).moveInitializeFrom(self._value, count: self._size)
+    }
+    
+    var objCType : UnsafePointer<Int8> {
+        get {
+            return self._typeInfo.withUnsafeMutablePointerToElements { (typep: UnsafeMutablePointer<Int8>) -> UnsafePointer<Int8> in
+                return unsafeBitCast(typep, UnsafePointer<Int8>.self)
+            }
+        }
+    }
+    
+    override var classForKeyedArchiver: AnyClass? {
+        return NSValue.self
+    }
+    
+    override var description : String {
+        return NSData.init(bytes: self.value, length: self._size).description
+    }
+    
+    convenience required init?(coder aDecoder: NSCoder) {
+        if !aDecoder.allowsKeyedCoding {
+            NSUnimplemented()
+        } else if aDecoder.containsValueForKey("NS.special") {
+            NSUnimplemented()
+        } else {
+            guard let type = aDecoder.decodeObject() as? NSString else {
+                return nil
+            }
+            
+            let typep = type._swiftObject
+            
+            self.init(bytes: nil, objCType: typep)
+            aDecoder.decodeValueOfObjCType(typep, at: self.value)
+        }
+    }
+    
+    static func supportsSecureCoding() -> Bool {
+        return true
+    }
+
+    func encodeWithCoder(aCoder: NSCoder) {
+        if !aCoder.allowsKeyedCoding {
+            NSUnimplemented()
+        } else if self._specialFlags != .NotSpecialType {
+            NSUnimplemented()
+        } else {
+            aCoder.encodeObject(String.fromCString(self.objCType)!.bridge())
+            aCoder.encodeValueOfObjCType(self.objCType, at: self.value)
+        }
+    }
+    
+    private var _size : Int {
+        return self._typeInfo.value.size
+    }
+    
+    private var value : UnsafeMutablePointer<Void> {
+        return unsafeBitCast(self._value, UnsafeMutablePointer<Void>.self)
+    }
+    
+    override func copy() -> AnyObject {
+        return copyWithZone(nil)
+    }
+    
+    func copyWithZone(zone: NSZone) -> AnyObject {
+        return self
+    }
+}
+
+internal func ==(x : NSConcreteValue, y : NSConcreteValue) -> Bool {
+    return x._typeInfo == y._typeInfo
+}
+
+internal func ==(x : NSConcreteValue.TypeInfoHeader, y : NSConcreteValue.TypeInfoHeader) -> Bool {
+    return x.name == y.name && x.size == y.size
+}
+
+internal func ==(x : NSConcreteValue.TypeInfo, y : NSConcreteValue.TypeInfo) -> Bool {
+    return x.value == y.value
+}
+
--- Foundation/NSValue.swift
+++ Foundation/NSValue.swift
@@ -7,19 +7,68 @@
 // See http://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 //
 
-
 public class NSValue : NSObject, NSCopying, NSSecureCoding, NSCoding {
-    
+
+    private static var SideTable = [ObjectIdentifier : NSConcreteValue]()
+    private static var SideTableLock = NSLock()
+
     internal override init() {
-        
+        super.init()
+        // on Darwin [NSValue new] returns nil
+    }
+    
+    deinit {
+        if self.dynamicType == NSValue.self {
+            NSValue.SideTableLock.synchronized {
+                NSValue.SideTable[ObjectIdentifier(self)] = nil
+            }
+        }
+    }
+    
+    public func getValue(value: UnsafeMutablePointer<Void>) {
+        if self.dynamicType == NSValue.self {
+            NSValue.SideTableLock.synchronized {
+                NSValue.SideTable[ObjectIdentifier(self)]!.getValue(value)
+            }
+        } else {
+            NSRequiresConcreteImplementation()
+        }
+    }
+    
+    public var objCType: UnsafePointer<Int8> {
+        if self.dynamicType == NSValue.self {
+            return NSValue.SideTableLock.synchronized {
+                return NSValue.SideTable[ObjectIdentifier(self)]!.objCType
+            }
+        } else {
+            NSRequiresConcreteImplementation()
+        }
+    }
+    
+    public convenience required init(bytes value: UnsafePointer<Void>, objCType type: UnsafePointer<Int8>) {
+        if self.dynamicType == NSValue.self {
+            self.init()
+            NSValue.SideTableLock.synchronized {
+                NSValue.SideTable[ObjectIdentifier(self)] = NSConcreteValue(bytes: value, objCType: type)
+            }
+        } else {
+            NSRequiresConcreteImplementation()
+        }
     }
     
     public required init?(coder aDecoder: NSCoder) {
-        
+        // no need to check concrete map as this is delegated via classForKeyedArchiver
+        NSRequiresConcreteImplementation()
     }
     
     public func encodeWithCoder(aCoder: NSCoder) {
-        
+        if self.dynamicType == NSValue.self {
+            NSValue.SideTableLock.synchronized {
+                NSValue.SideTable[ObjectIdentifier(self)]!.encodeWithCoder(aCoder)
+            }
+        } else {
+            NSRequiresConcreteImplementation()
+        }
     }
     
     public static func supportsSecureCoding() -> Bool {
@@ -33,5 +82,11 @@ public class NSValue : NSObject, NSCopying, NSSecureCoding, NSCoding {
     public func copyWithZone(zone: NSZone) -> AnyObject {
         return self
     }
+    
+    public override class func classForKeyedUnarchiver() -> AnyClass {
+        return NSConcreteValue.self
+    }
+    
+    
 }
 
